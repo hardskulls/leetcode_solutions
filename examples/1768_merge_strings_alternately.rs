8f6c689f1e445709099c8846442c45b8b8e9a5c9
hardskulls <hardskulls@rambler.ru>
2023-06-29 09:35:27 +0300

    #[✨style (347_top_k_frequent_elements, 1768_merge_strings_alternately)] Updated formatting.

--- examples/1768_merge_strings_alternately.rs
+++ examples/1768_merge_strings_alternately.rs
@@ -1,33 +1,35 @@
 fn main() {}
 
-fn solution(word_1: impl AsRef<str>, word_2: impl AsRef<str>) -> String {
+fn solution(word_1: impl AsRef<str>, word_2: impl AsRef<str>) -> String
+{
     let (word_1, word_2) = (word_1.as_ref(), word_2.as_ref());
 
     let mut buffer = String::with_capacity(word_1.len() + word_2.len());
     let max_length = std::cmp::max(word_1.len(), word_2.len());
 
-    for i in 0..max_length {
+    for i in 0..max_length
+    {
         let next = i + 1;
 
-        if let Some(s) = word_1.get(i..next) {
-            buffer.push_str(s)
-        }
+        if let Some(s) = word_1.get(i..next)
+        { buffer.push_str(s) }
 
-        if let Some(s) = word_2.get(i..next) {
-            buffer.push_str(s)
-        }
+        if let Some(s) = word_2.get(i..next)
+        { buffer.push_str(s) }
     }
 
     buffer
 }
 
 #[cfg(test)]
-mod tests {
+mod tests
+{
     use crate::solution;
     use leetcode_solutions::benchmarking::bench_times;
 
     #[test]
-    fn solution_test() {
+    fn solution_test()
+    {
         let (words_1, words_2) = ("a", "b");
         let expect = "ab".to_owned();
         let res = solution(words_1, words_2);
@@ -38,10 +40,11 @@ mod tests {
         let res = solution(words_1, words_2);
         assert_eq!(res, expect);
 
-        let (words_1, words_2) = (
-            "khgkutdyrstrdjytgfkjhjk",
-            "lohulikjhfuytdyyfytsturstrurtdyjtdy",
-        );
+        let (words_1, words_2) =
+            (
+                "khgkutdyrstrdjytgfkjhjk",
+                "lohulikjhfuytdyyfytsturstrurtdyjtdy",
+            );
         let expect = "klhoghkuultidkyjrhsfturydtjdyytygffyktjshtjukrstrurtdyjtdy".to_owned();
         assert_eq!(solution(words_1, words_2), expect);
     }
--- examples/347_top_k_frequent_elements.rs
+++ examples/347_top_k_frequent_elements.rs
@@ -2,12 +2,12 @@ use std::collections::HashMap;
 
 fn main() {}
 
-fn solution(nums: Vec<i32>, k: i32) -> Vec<i32> {
+fn solution(nums: Vec<i32>, k: i32) -> Vec<i32>
+{
     let mut stor = HashMap::new();
 
     let update = |n| *stor.entry(n).or_insert(0) += 1;
-    nums.into_iter()
-        .for_each(update);
+    nums.into_iter().for_each(update);
 
     let mut stor: Vec<_> = stor.into_iter().collect();
     stor.sort_by_key(|(_k, v)| *v);
@@ -20,11 +20,13 @@ fn solution(nums: Vec<i32>, k: i32) -> Vec<i32> {
 }
 
 #[cfg(test)]
-mod tests {
+mod tests
+{
     use crate::solution;
 
     #[test]
-    fn solution_test() {
+    fn solution_test()
+    {
         assert_eq!(solution(vec![0, 5, 4, 2, 1, 6, 6], 1), vec![6]);
     }
 }
